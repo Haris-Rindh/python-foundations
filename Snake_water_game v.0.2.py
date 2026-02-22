7747ed17f83cf6041bef07d258667d5c4c649459
Haris Rindh <haris.rindh.pk@gmail.com>
2026-02-22 12:32:30 +0500

    Snake_water_game v.0.2

---
+++ Snake_water_game v.0.2.py
@@ -0,0 +1,25 @@
+import random
+# computer chose any random number
+computer = random.choice([0, 1, 2])
+#Ask user choice 
+print('Keys: S = Snake, W = Water, G = Gun')
+you = input("Enter your Choice: ").lower()
+#It assign keys to the values
+youDict = {"s": 0, "w": 1, "g": 2}
+reverseDict = {0: "Snake", 1: "Water", 2: "Gun"}
+
+#It check user input is valid or not
+if you not in youDict:
+    print("Invalid Choice!")
+    exit()
+
+youchose = youDict[you]
+#It tell us user and computer choice
+print(f"You Chose: {reverseDict[youchose]}\nComputer Chose: {reverseDict[computer]}")
+
+if computer == youchose:
+    print("It's a Draw!")
+elif (computer - youchose == -1 or computer - youchose == 2):
+    print("You Lose!")
+else:
+    print("You Win!")
